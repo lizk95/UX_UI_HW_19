be18a3be131359ca7804bfa4764b467344f7ec38
lizk95 <elizabethkilgore15@gmail.com>
2023-02-06 19:37:59 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ let button = document.querySelector;
 button.addEventListener('mouseover', function(){
   button.style.background = "#34A853";
 });
-
 button.addEventListener('mouseout', function(){
   button.style.background = "#0f500f";
 });
