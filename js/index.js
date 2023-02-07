533f1fc6029d5910a3e5dc641cd76c7618fcb688
lizk95 <elizabethkilgore15@gmail.com>
2023-02-06 19:34:18 -0500

    update js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
+let button = document.querySelector;
+button.addEventListener('mouseover', function(){
+  button.style.background = "#34A853";
+});
 
+button.addEventListener('mouseout', function(){
+  button.style.background = "#0f500f";
+});
